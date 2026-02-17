c4c5654ed082ca82342940e8dc8996170352e046
Andri Quinn <dnlndrgntqntcn@gmail.com>
2026-02-17 10:01:06 +0800

    Refactor

--- app/src/main/java/com/quinn/to_do_list/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/quinn/to_do_list/viewmodel/HomeViewModel.kt
@@ -1,5 +1,8 @@
 package com.quinn.to_do_list.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.quinn.to_do_list.data.local.entity.Tasks
@@ -10,12 +13,25 @@ import kotlinx.coroutines.launch
 class HomeViewModel(
     private val repository: TaskRepository) : ViewModel() {
 
+    var taskName by  mutableStateOf("")
+        private set
     val tasks: Flow<List<Tasks>> = repository.readAllTask()
 
-    fun addTask(taskName: String) {
+
+    fun onTextChange(newText: String) {
+        taskName = newText
+    }
+
+    fun addTask() {
+        if (taskName.isEmpty()) {
+            taskName = "Untitled Task"
+        }
+
         viewModelScope.launch {
             repository.addTask(taskName)
         }
+
+        taskName = ""
     }
 
     fun removeTask(task: Tasks) {
